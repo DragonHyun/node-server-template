53a1b7a46f040ca01f0b7d44ae4fdd61bbb62172
DragonHyun <DragonHyunLee@gmail.com>
2021-04-07 11:38:02 +0000

    update template

--- Controllers/IndexController.js
+++ Controllers/IndexController.js
@@ -1,63 +1,60 @@
-const success = (req, res, next) => {
-    res.status(200).json(
-        {
-            "success" : true,
-            "message" : "You make success"
+const indexController = {
+    success: (req, res, next) => {
+        res.status(200).json(
+            {
+                "success" : true,
+                "message" : "You make success"
+            }
+        );
+    },
+
+    paramName: (req, res, next) => {
+        const name = req.params.name;
+
+        if(!name) {
+            return res.status(400).json({ err: '이름 없음' });
         }
-    );
-}
 
-const paramName = (req, res, next) => {
-    const name = req.params.name;
+        res.status(200).json(
+            {
+                "success" : true,
+                "message" : 'param 예제',
+                "result" : `Hello ${name}`
+            }
+        )
+    },
 
-    if(!name) {
-        return res.status(400).json({ err: '이름 없음' });
-    }
+    querystring: (req, res, next) => {
+        const queryString = req.query.name;
 
-    res.status(200).json(
-        {
-            "success" : true,
-            "message" : 'param 예제',
-            "result" : `Hello ${name}`
+        if(!queryString) {
+            return res.status(400).json({ err: 'queryString 없음'});
         }
-    )
-}
-
-const querystring = (req, res, next) => {
-    const queryString = req.query.name;
-
-    if(!queryString) {
-        return res.status(400).json({ err: 'queryString 없음'});
-    }
     
-    res.status(200).json(
-        {
-            "success" : true,
-            "message" : 'queryString 예제',
-            "result" : queryString
+        res.status(200).json(
+            {
+                "success" : true,
+                "message" : 'queryString 예제',
+                "result" : queryString
+            }
+        )
+    },
+
+    message: (req, res, next) => {
+        const message = req.body.message || '';
+
+        if(!message.length){
+            return res.status(400).json({ err: 'There are no message' });
         }
-    )
+
+        res.status(200).json(
+            {
+                "success" : true,
+                "message" : "메시지를 잘 받았습니다",
+                "result" : message
+            }
+        )
+    },
 }
 
-const message = (req, res, next) => {
-    const message = req.body.message || '';
-
-    if(!message.length){
-        return res.status(400).json({ err: 'There are no message' });
-    }
-
-    res.status(200).json(
-        {
-            "success" : true,
-            "message" : "메시지를 잘 받았습니다",
-            "result" : message
-        }
-    )
-}
-
-module.exports = {
-    success,
-    paramName,
-    querystring,
-    message,
-}
+module.exports = indexController;
